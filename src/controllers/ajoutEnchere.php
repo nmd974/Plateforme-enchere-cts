855a1914190c803c3db0eb5d91adf7077feb263d
nmd974 <jha.payet@gmail.com>
2020-12-03 12:08:41 +0400

    ajout de la fonction d'ajout du dossier image si inexistant

--- src/controllers/ajoutEnchere.php
+++ src/controllers/ajoutEnchere.php
@@ -8,21 +8,24 @@
     {
     //Faire le contrôle des images
         if($image_upload['image_upload']['size'] !== 0){
-            $validationImage = controleImage($image_upload);
-            if(!$validationImage[0]){//Si le contrôle du type, taille est ok alors on poursuit
-                $obj = new AjoutEnchere (
-                    $data['intitule'],
-                    $data['prix_depart'],
-                    $data['duree_enchere'],
-                    $validationImage[1],//Correspond au nom de l'image lors de la validation de la fonction imagecontrol
-                    $data['prix_clic'],
-                    $data['augmentation_prix'],
-                    $data['augmentation_duree']
-                );
-                return '<div class="alert alert-success"> Le produit a bien été ajouté !</div>';
-            }else{
-                return $validationImage[1];
-            };
+            $dir_exist = verificationDossierImg();
+            if($dir_exist){
+                $validationImage = controleImage($image_upload);
+                if(!$validationImage[0]){//Si le contrôle du type, taille est ok alors on poursuit
+                    $obj = new AjoutEnchere (
+                        $data['intitule'],
+                        $data['prix_depart'],
+                        $data['duree_enchere'],
+                        $validationImage[1],//Correspond au nom de l'image lors de la validation de la fonction imagecontrol
+                        $data['prix_clic'],
+                        $data['augmentation_prix'],
+                        $data['augmentation_duree']
+                    );
+                    return '<div class="alert alert-success"> Le produit a bien été ajouté !</div>';
+                }else{
+                    return $validationImage[1];
+                };
+            }
         }else{
             $obj = new AjoutEnchere (
                 $data['intitule'],
@@ -35,6 +38,16 @@
             );
             return '<div class="alert alert-success"> Le produit a bien été ajouté !</div>';
         }
+    }
+
+    function verificationDossierImg():bool
+    {
+        //On ajoute le dossier image si le dossier n'existe pas
+        $dir = __ROOT__."/img";
+        if (!is_dir($dir)){
+            mkdir($dir, 0777, true);
+        }
+        return is_dir($dir);
     }
 
 ?>
