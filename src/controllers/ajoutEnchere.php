9c8bc3c048dafa826fd56f70321543effa49f700
nmd974 <jha.payet@gmail.com>
2020-12-08 08:22:47 +0400

    ajout securisation formulaire

--- src/controllers/ajoutEnchere.php
+++ src/controllers/ajoutEnchere.php
@@ -6,38 +6,60 @@
 <?php 
     function validationAjout($data, $image_upload):string
     {
-    //Faire le contrôle des images
-        if($image_upload['image_upload']['size'] !== 0){
-            $dir_exist = verificationDossierImg();
-            if($dir_exist){
-                $validationImage = controleImage($image_upload);
-                if(!$validationImage[0]){//Si le contrôle du type, taille est ok alors on poursuit
-                    $obj = new AjoutEnchere (
-                        $data['intitule'],
-                        $data['prix_depart'],
-                        $data['duree_enchere'],
-                        $validationImage[1],//Correspond au nom de l'image lors de la validation de la fonction imagecontrol
-                        $data['prix_clic'],
-                        $data['augmentation_prix'],
-                        $data['augmentation_duree']
-                    );
-                    return '<div class="alert alert-success"> Le produit a bien été ajouté !</div>';
-                }else{
-                    return $validationImage[1];
-                };
+        $inputsRequired=["intitule", "prix_depart","duree_enchere","prix_clic","augmentation_prix","augmentation_duree"];
+        $inputsNumbers = ["prix_depart","duree_enchere","prix_clic","augmentation_prix","augmentation_duree"];
+        $validationForms = false;
+        foreach($inputsRequired as $input){
+            if($data["$input"] == ""){
+                $validationForms = false;
+            }else{
+                $validationForms = true;
+            }
+        };
+        foreach($inputsNumbers as $input){
+            if($data["$input"] <= 0){
+                $validationForms = false;
+            }else{
+                $validationForms = true;
+            }
+        };
+        if($validationForms){
+            //Faire le contrôle des images
+            if($image_upload['image_upload']['size'] !== 0){
+                $dir_exist = verificationDossierImg();
+                if($dir_exist){
+                    $validationImage = controleImage($image_upload);
+                    if(!$validationImage[0]){//Si le contrôle du type, taille est ok alors on poursuit
+                        $obj = new AjoutEnchere (
+                            $data['intitule'],
+                            $data['prix_depart'],
+                            $data['duree_enchere'],
+                            $validationImage[1],//Correspond au nom de l'image lors de la validation de la fonction imagecontrol
+                            $data['prix_clic'],
+                            $data['augmentation_prix'],
+                            $data['augmentation_duree']
+                        );
+                        return '<div class="alert alert-success"> Le produit a bien été ajouté !</div>';
+                    }else{
+                        return $validationImage[1];
+                    };
+                }
+            }else{
+                $obj = new AjoutEnchere (
+                    $data['intitule'],
+                    $data['prix_depart'],
+                    $data['duree_enchere'],
+                    null,//Correspond à rien si pas d'image ajoutee
+                    $data['prix_clic'],
+                    $data['augmentation_prix'],
+                    $data['augmentation_duree']
+                );
+                return '<div class="alert alert-success"> Le produit a bien été ajouté !</div>';
             }
         }else{
-            $obj = new AjoutEnchere (
-                $data['intitule'],
-                $data['prix_depart'],
-                $data['duree_enchere'],
-                null,//Correspond à rien si pas d'image ajoutee
-                $data['prix_clic'],
-                $data['augmentation_prix'],
-                $data['augmentation_duree']
-            );
-            return '<div class="alert alert-success"> Le produit a bien été ajouté !</div>';
+            return '<div class="alert alert-danger">Le formulaire est erroné !</div>';
         }
+        
     }
 
     function verificationDossierImg():bool
--- src/controllers/modificationEnchere.php
+++ src/controllers/modificationEnchere.php
@@ -5,52 +5,72 @@
     //Si elle n'a pas change on ne verifie pas l'image sinon on modifie l'image
     function modificationEnchere($content, $image, $nomOldImage):string
     {
-        if($image['image_upload']['size'] !== 0)//S'il y a un nom d'image c'est que l'utilisateur souhaite changer l'image et a ajouté une nouvelle image
-        {
-            //On supprime l'ancienne image attribuée si ce n'est pas egal à null
-            if($nomOldImage !== ""){
-                $oldFilename = __ROOT__."/img/" . $nomOldImage;
-                unlink($oldFilename);
+        $inputsRequired=["intitule", "prix_depart","duree_enchere","prix_clic","augmentation_prix","augmentation_duree"];
+        $inputsNumbers = ["prix_depart","duree_enchere","prix_clic","augmentation_prix","augmentation_duree"];
+        $validationForms = false;
+        foreach($inputsRequired as $input){
+            if($data["$input"] == ""){
+                $validationForms = false;
+            }else{
+                $validationForms = true;
             }
-            //On lance cette fonction pour pouvoir gérer l'image ajoutée
-            $image_upload = controleImage($image);
-
-            $enregistrementData = json_decode(file_get_contents(__ROOT__.'/src/data/data.json'), true); //On recupere le contenu de data.json
-            foreach($enregistrementData as $key => $items){
-                if ($items['id'] == $content['id']){
-                    $enregistrementData[$key]['intitule'] = $content['intitule'];
-                    $enregistrementData[$key]['prix_depart'] = $content['prix_depart'];
-                    $enregistrementData[$key]['image_nom'] = $image_upload[1];
-                    $enregistrementData[$key]['duree_enchere'] = $content['duree_enchere'];
-                    $enregistrementData[$key]['prix_clic'] = $content['prix_clic'];
-                    $enregistrementData[$key]['augmentation_prix'] = $content['augmentation_prix'];
-                    $enregistrementData[$key]['augmentation_duree'] = $content['augmentation_duree'];
+        };
+        foreach($inputsNumbers as $input){
+            if($data["$input"] <= 0){
+                $validationForms = false;
+            }else{
+                $validationForms = true;
+            }
+        };
+        if($validationForms){
+            if($image['image_upload']['size'] !== 0)//S'il y a un nom d'image c'est que l'utilisateur souhaite changer l'image et a ajouté une nouvelle image
+            {
+                //On supprime l'ancienne image attribuée si ce n'est pas egal à null
+                if($nomOldImage !== ""){
+                    $oldFilename = __ROOT__."/img/" . $nomOldImage;
+                    unlink($oldFilename);
                 }
+                //On lance cette fonction pour pouvoir gérer l'image ajoutée
+                $image_upload = controleImage($image);
+    
+                $enregistrementData = json_decode(file_get_contents(__ROOT__.'/src/data/data.json'), true); //On recupere le contenu de data.json
+                foreach($enregistrementData as $key => $items){
+                    if ($items['id'] == $content['id']){
+                        $enregistrementData[$key]['intitule'] = $content['intitule'];
+                        $enregistrementData[$key]['prix_depart'] = $content['prix_depart'];
+                        $enregistrementData[$key]['image_nom'] = $image_upload[1];
+                        $enregistrementData[$key]['duree_enchere'] = $content['duree_enchere'];
+                        $enregistrementData[$key]['prix_clic'] = $content['prix_clic'];
+                        $enregistrementData[$key]['augmentation_prix'] = $content['augmentation_prix'];
+                        $enregistrementData[$key]['augmentation_duree'] = $content['augmentation_duree'];
+                    }
+                }
+                file_put_contents(__ROOT__.'/src/data/data.json', json_encode($enregistrementData));
+                return 
+                '<div class="col-12 d-flex justify-content-center">
+                <div class="alert alert-success">Le produit a bien été modifié !</div>
+                </div>'; 
             }
-            file_put_contents(__ROOT__.'/src/data/data.json', json_encode($enregistrementData));
-            return 
-            '<div class="col-12 d-flex justify-content-center">
-            <div class="alert alert-success">Le produit a bien été modifié !</div>
-            </div>'; 
+            else
+            {
+                $enregistrementData = json_decode(file_get_contents(__ROOT__.'/src/data/data.json'), true); //On recupere le contenu de data.json
+                foreach($enregistrementData as $key => $items){
+                    if ($items['id'] == $content['id']){
+                        $enregistrementData[$key]['intitule'] = $content['intitule'];
+                        $enregistrementData[$key]['prix_depart'] = $content['prix_depart'];
+                        $enregistrementData[$key]['duree_enchere'] = $content['duree_enchere'];
+                        $enregistrementData[$key]['prix_clic'] = $content['prix_clic'];
+                        $enregistrementData[$key]['augmentation_prix'] = $content['augmentation_prix'];
+                        $enregistrementData[$key]['augmentation_duree'] = $content['augmentation_duree'];
+                    }
+                }
+                file_put_contents(__ROOT__.'/src/data/data.json', json_encode($enregistrementData));
+                        return 
+                        '<div class="col-12 d-flex justify-content-center">
+                        <div class="alert alert-success">Le produit a bien été modifié !</div>
+                        </div>';  
+                }
         }
-        else
-        {
-            $enregistrementData = json_decode(file_get_contents(__ROOT__.'/src/data/data.json'), true); //On recupere le contenu de data.json
-            foreach($enregistrementData as $key => $items){
-                if ($items['id'] == $content['id']){
-                    $enregistrementData[$key]['intitule'] = $content['intitule'];
-                    $enregistrementData[$key]['prix_depart'] = $content['prix_depart'];
-                    $enregistrementData[$key]['duree_enchere'] = $content['duree_enchere'];
-                    $enregistrementData[$key]['prix_clic'] = $content['prix_clic'];
-                    $enregistrementData[$key]['augmentation_prix'] = $content['augmentation_prix'];
-                    $enregistrementData[$key]['augmentation_duree'] = $content['augmentation_duree'];
-                }
-            }
-            file_put_contents(__ROOT__.'/src/data/data.json', json_encode($enregistrementData));
-                    return 
-                    '<div class="col-12 d-flex justify-content-center">
-                    <div class="alert alert-success">Le produit a bien été modifié !</div>
-                    </div>';  
-            }
+        
     }
 ?>
